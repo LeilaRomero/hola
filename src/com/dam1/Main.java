9dd9a7e9d17e40d3558e702b5a5a36d74fa5a952
Leila Romero Fajar <lromerofajar@danielcastelao.org>
2019-11-08 14:20:04 +0100

    hola mundo

--- src/com/dam1/Main.java
+++ src/com/dam1/Main.java
@@ -3,6 +3,8 @@ package com.dam1;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hola mundo");
+        System.out.println("hola mundo desde aqui");
+        //TODO
+        //Comprobar cosas
     }
 }
